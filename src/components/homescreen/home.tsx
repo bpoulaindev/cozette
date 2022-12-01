d57273064db3b952149c1d86e688c519ae666f1e
Baptiste Poulain <baptiste.poulain59@gmail.com>
2022-12-01 23:28:55 +0100

    (aws_S3): link to aws, use camera and gallery through expo

--- src/components/homescreen/home.tsx
+++ src/components/homescreen/home.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTailwind } from 'tailwind-rn';
-import { View, Image, ScrollView } from 'react-native';
+import { View, Image, ScrollView, TextInput, Pressable } from 'react-native';
 import { SimpleButton } from '../../design/Buttons';
 import { AppText } from '../../design/Text';
-import { MapPinIcon, StarIcon } from 'react-native-heroicons/solid';
+import { MapPinIcon, StarIcon, ArrowDownTrayIcon } from 'react-native-heroicons/solid';
+import {
+  S3Client,
+  ListObjectsCommand,
+  PutObjectCommand,
+  GetObjectCommand
+} from '@aws-sdk/client-s3';
+import 'react-native-url-polyfill/auto';
+import 'react-native-get-random-values';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+
+import axios from 'axios';
+import { ImagePickerResult } from 'expo-image-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const Home = () => {
   const tailwind = useTailwind();
@@ -35,6 +49,110 @@ export const Home = () => {
       }
     }
   ];
+  const [fileName, setFileName] = useState<string>('');
+  const [successMsg, setSuccessMsg] = useState<string>('');
+  const [errorMsg, setErrorMsg] = useState<string>('');
+  const [fileBuffer, setFileBuffer] = useState<Blob>();
+  const [fileType, setFileType] = useState<string>('');
+
+  // Replace REGION with the appropriate AWS Region, such as 'us-east-1'.
+  const region = 'eu-west-3';
+  const client = new S3Client({
+    region,
+    credentials: {
+      accessKeyId: 'AKIAZCFFD2IVEPK2RUVR',
+      secretAccessKey: 'jezQAiSladAAvrizOnhVmyPGQCE0DbIDhkzuGspX'
+    }
+    /*credentials: fromCognitoIdentityPool({
+      client: new CognitoIdentityClient({ region }),
+      // Replace IDENTITY_POOL_ID with an appropriate Amazon Cognito Identity Pool ID for, such as 'us-east-1:xxxxxx-xxx-4103-9936-b52exxxxfd6'.
+      identityPoolId: 'us-east-1:34987c2b-0f76-4347-a742-d4b3bfae065d'
+    }) */
+  });
+  const [listBucketFiles, setListBucketFiles] = useState<unknown[]>([]);
+  const putBucketObject = async () => {
+    setSuccessMsg('');
+    setErrorMsg('');
+
+    try {
+      await client.send(
+        new PutObjectCommand({
+          Body: `${fileBuffer || fileName}.${fileType}`,
+          Bucket: 'mds-cloudcomputing',
+          Key: fileName
+        })
+      );
+      setSuccessMsg(`File has been sent.`);
+      setFileName('');
+    } catch (e: any) {
+      setErrorMsg(e);
+    }
+  };
+  const listBucket = async () => {
+    try {
+      const listBucket = await client.send(
+        new ListObjectsCommand({ Bucket: 'mds-cloudcomputing' })
+      );
+      setSuccessMsg(
+        `Number of files: ${listBucket.Contents?.length ? listBucket.Contents.length - 1 : 0}`
+      );
+      setListBucketFiles(listBucket.Contents ?? []);
+      console.log(listBucket);
+    } catch (e: any) {
+      setErrorMsg(e);
+    }
+  };
+  const getBucketObject = async (key: string) => {
+    try {
+      const data = await client.send(
+        new GetObjectCommand({
+          Bucket: 'mds-cloudcomputing',
+          Key: key
+        })
+      );
+      // const stream = await data.Body.transformToString();
+      console.log('test2', data);
+      // console.log(response);
+      // const file = await fetch(response);
+      // console.log('object', object);
+    } catch (e: any) {
+      setErrorMsg(e);
+    }
+  };
+  const pickDocument = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync();
+    await uploadPicture(result);
+  };
+  const openCamera = async () => {
+    const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+    if (!permissionResult.granted) {
+      alert("You've refused to allow this appp to access your camera!");
+      return;
+    }
+    const result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All
+    });
+    console.log(result);
+    await uploadPicture(result);
+  };
+  const uploadPicture = async (result: ImagePickerResult) => {
+    if (!result.cancelled && result.assets) {
+      const picture = await axios.get(result.assets[0].uri);
+      setFileBuffer(picture.data);
+      setFileType(picture.data.type);
+      console.log(result.assets[0].uri);
+    }
+  };
+  const downloadFile = async (uri: string) => {
+    FileSystem.downloadAsync('uri', FileSystem.documentDirectory + 'small.mp4')
+      .then(({ uri }) => {
+        console.log('Finished downloading to ', uri);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <ScrollView
       style={tailwind(' w-full h-auto')}
@@ -81,6 +199,82 @@ export const Home = () => {
           </View>
         </View>
         <View style={tailwind('mt-4 lg:mt-8 max-w-[85%] w-full flex flex-col')}>
+          <View style={tailwind('flex w-full my-4')}>
+            <AppText style={{ color: 'green' }}>
+              {successMsg ? `Success: ${successMsg}` : ``}
+            </AppText>
+            <AppText style={{ color: 'red' }}>{errorMsg ? `Error: ${errorMsg}` : ``}</AppText>
+            <View>
+              <View style={tailwind('w-full rounded-md mt-1/2 md:mt-2 bg-white w-full')}>
+                <TextInput
+                  style={tailwind(
+                    'py-1 md:py-2 px-3 lg:py-4 lg:px-6 text-sm lg:text-base leading-5'
+                  )}
+                  onChangeText={(text) => setFileName(text)}
+                  autoCapitalize={'none'}
+                  value={fileName}
+                  placeholder={'Enter File Name'}
+                />
+              </View>
+              <SimpleButton
+                buttonClasses='mt-2'
+                variant='contained'
+                color='primary'
+                content='Put Bucket object'
+                onPress={putBucketObject}
+              />
+              <SimpleButton
+                buttonClasses='mt-2'
+                variant='contained'
+                color='light'
+                content='List Bucket objects'
+                onPress={listBucket}
+              />
+              <SimpleButton
+                buttonClasses='mt-2'
+                variant='contained'
+                color='light'
+                content='File input'
+                onPress={pickDocument}
+              />
+              <SimpleButton
+                buttonClasses='mt-2'
+                variant='contained'
+                color='light'
+                content='Camera'
+                onPress={openCamera}
+              />
+              {listBucketFiles.length > 0 && (
+                <AppText style={tailwind('text-base font-normal my-2')} font='LatoBold'>
+                  Results:
+                </AppText>
+              )}
+              {listBucketFiles.map((file: any, index) => (
+                <View
+                  style={tailwind(
+                    `flex flex-row justify-between items-center w-full mt-2 pb-1 ${
+                      index !== listBucketFiles.length && 'border-b-[.5px] border-gray-200'
+                    }`
+                  )}
+                  key={index}>
+                  <AppText style={tailwind('flex max-w-[70%]')}>{file.Key}</AppText>
+                  <Pressable
+                    accessibilityLabel='HomeButton'
+                    onPress={() => getBucketObject(file.Key)}>
+                    {({ pressed }) => (
+                      <ArrowDownTrayIcon
+                        style={tailwind(
+                          `${
+                            pressed ? 'text-primary-300' : 'text-primary-100'
+                          } max-h-[25px] max-w-[25px] lg:max-h-[28px] lg:max-w-[28px] xl:max-h-[30px] xl:max-w-[30px]`
+                        )}
+                      />
+                    )}
+                  </Pressable>
+                </View>
+              ))}
+            </View>
+          </View>
           <View style={tailwind('flex flex-row w-full justify-between items-center')}>
             <AppText style={tailwind('text-.5xl')} font='Superior'>
               Les nouveaux lieux
